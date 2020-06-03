7db217e59f77a09448a6ea7065ba726f0e281c2c
Tim <nerdycoder@gmail.com>
2020-06-03 12:32:17 +0300

    Initial Commit

---
+++ RingProgress/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RingProgress
+//
+//  Created by Tim Isaev on 03.06.2020.
+//  Copyright © 2020 Tim Isaev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
